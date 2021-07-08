b40c364a004fcc04656f5ed1c4eae06105cfae7c
jamesgnt <giangnhattruong@gmail.com>
2021-07-08 18:42:36 +0700

    config contact form back end, fix about and home main overflow to auto

--- adora.js
+++ adora.js
@@ -1,23 +1,55 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
+const flash = require("connect-flash");
+const session = require("express-session");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
 const ExpressError = require("./ultils/ExpressError");
 const wrapAsync = require("./ultils/wrapAsync");
 const { validateContact } = require("./middleware");
+const secret = process.env.SECRET || "simplesectionsecret";
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
+const sessionOptions = {
+  name: "in_mkto", // set random name for sessionID
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    // secure: true,    // this should only turn on when we use HTTPS
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionOptions));
+
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 app.use(
   mongoSanitize({
     replaceWith: "_",
   })
 );
+
 app.use(helmet());
 const scriptSrcUrls = [
   "https://stackpath.bootstrapcdn.com/",
@@ -64,16 +96,63 @@ app.use(
   })
 );
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587 || 2525,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
 app.get("/", (req, res) => {
-  res.render("home", {req});
+  res.render("home", { req });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", {req});
+  res.render("about", { req });
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact", {req});
+  res.render("contact", { req });
+});
+
+app.post("/send", (req, res) => {
+  //1.
+  let form = new multiparty.Form();
+  let data = {};
+  form.parse(req, function (err, fields) {
+    console.log(fields);
+    Object.keys(fields).forEach(function (property) {
+      data[property] = fields[property].toString();
+    });
+
+    //2. You can configure the object however you want
+    const mail = {
+      from: data.name,
+      to: process.env.EMAIL,
+      subject: "Request from Adora finance user",
+      text: `${data.name} <${data.email}> \n${data.message}`,
+    };
+
+    //3.
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Something went wrong.");
+      } else {
+        res.status(200).send("Email successfully sent to recipient!");
+      }
+    });
+  });
 });
 
 // app.post("/contact", validateContact, wrapAsync((req, res) => {}));
@@ -83,10 +162,10 @@ app.all("*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    const { status = 500 } = err;
-    if (!err.message) err.message = "Something went wrong!";
-    res.status(status).render('error', { req, err })
-})
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Something went wrong!";
+  res.status(status).render("error", { req, err });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -1,11 +1,21 @@
 const form = document.querySelector("#contact-form");
 
 const formEvent = form.addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    //2.
-    let mail = new FormData(form);
-  
-    //3.
-    sendMail(mail);
-  })
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
+});
+
+const sendMail = (mail) => {
+  //1.
+  fetch("https://limitless-savannah-41564.herokuapp.com/send" || "https://adora.finance/send", {
+    method: "post", //2.
+    body: mail, //3.
+  }).then((response) => {
+    return response.json();
+  });
+};
