a28edc88d2db0fa36bad8a1ca3eb87ed57630c96
jamesgnt <giangnhattruong@gmail.com>
2021-07-08 22:09:59 +0700

    connect to mongo

--- adora.js
+++ adora.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const helmet = require("helmet");
@@ -14,6 +16,20 @@ const { validateContact } = require("./middleware");
 const ExpressError = require("./ultils/ExpressError");
 const { sendEmail } = require("./controllers/contact.js");
 const secret = process.env.SECRET || "simplesectionsecret";
+const dbUrl = process.env.DB_URL;
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+//Test connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -21,7 +37,16 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24*60*60,
+  crypto: {
+      secret
+  }
+});
+
 const sessionOptions = {
+  store,
   name: "in_mkto", // set random name for sessionID
   secret,
   resave: false,
@@ -34,8 +59,8 @@ const sessionOptions = {
   },
 };
 app.use(session(sessionOptions));
-
 app.use(flash());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
