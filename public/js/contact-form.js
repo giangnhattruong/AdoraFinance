0c446b8cdd6ff24b8cc4d9efc9e6fe732ba37cdc
jamesgnt <giangnhattruong@gmail.com>
2021-07-09 15:14:57 +0700

    update npm

--- public/js/contact-form.js
+++ public/js/contact-form.js
@@ -2,28 +2,22 @@ const form = document.querySelector("#contact-form");
 
 const formEvent = form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  //2.
   let mail = new FormData(form);
-  //3.
-  sendMail(mail);
+  sendEmail(mail);
   form.elements.name = "";
   form.elements.email = "";
   form.elements.message = "";
 });
 
-const sendMail = async (mail) => {
-  //1.
+const sendEmail = async (mail) => {
   try {
-  console.dir(`mail: ${mail}`)
-  await fetch("https://limitless-savannah-41564.herokuapp.com/send", {
-    method: "post", //2.
-    body: mail, //3.
-  }).then((response) => {
-    console.log(`Response: ${response.json()}`);
-    return response.json();
-  });
-} catch (error) {
-  console.log(error);
-}
+    await fetch("https://limitless-savannah-41564.herokuapp.com/send", {
+      method: "post",
+      body: mail,
+    }).then((response) => {
+      return response.json();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
