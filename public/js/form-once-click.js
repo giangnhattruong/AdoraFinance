c4d148d24779aff211afc41f76c2eba753411fb0
giangnhattruong <giangnhattruong@gmail.com>
2021-07-15 18:40:57 +0700

    fix search page with article snippet

--- public/js/form-once-click.js
+++ public/js/form-once-click.js
@@ -4,14 +4,3 @@ form.addEventListener('submit', function (e) {
     btn.disabled = true;
     return this.submit();
 })
-
-// (function () {
-//     const form = document.querySelector('form')
-//     let allowSubmit = true;
-//     form.onsubmit = function () {
-//        if (allowSubmit)
-//            allowSubmit = false;
-//        else 
-//            return false;
-//     }
-// })();
