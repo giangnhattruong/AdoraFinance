29b99e77b77f7a45998017cb5aa78509b3f3de1d
Giang Nhat Truong (James) <giangnhattruong@gmail.com>
2021-12-03 14:06:24 +0700

    First commit to branch

--- adora.js
+++ adora.js
@@ -1,69 +1,71 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
 }
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const MongoStore = require("connect-mongo");
-const path = require("path");
-const ejsMate = require("ejs-mate");
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const path = require('path');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const flash = require("connect-flash");
-const session = require("express-session");
-const ExpressError = require("./ultils/ExpressError");
-const secret = process.env.SECRET || "simplesessionsecret";
-const dbUrl = `mongodb+srv://${process.env.DB_CRE}@aracluster.gyctd.mongodb.net/araDatabase?retryWrites=true&w=majority`;
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const flash = require('connect-flash');
+const session = require('express-session');
+const ExpressError = require('./ultils/ExpressError');
+const secret = process.env.SECRET || 'simplesessionsecret';
+const dbUrl = process.env.DB_CRE
+	? `mongodb+srv://${process.env.DB_CRE}@aracluster.gyctd.mongodb.net/araDatabase?retryWrites=true&w=majority`
+	: 'mongodb://127.0.0.1:27017/test';
 const adminId = process.env.ADORA_ADMIN_ID;
-const mainRoutes = require("./routes/main.js");
-const articleRoutes = require("./routes/articles.js");
-const userRoutes = require("./routes/users.js");
-const apiRoutes = require("./routes/circSupply.js");
+const mainRoutes = require('./routes/main.js');
+const articleRoutes = require('./routes/articles.js');
+const userRoutes = require('./routes/users.js');
+const apiRoutes = require('./routes/circSupply.js');
 const User = require('./models/users.js');
 
 mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
+	useNewUrlParser    : true,
+	useCreateIndex     : true,
+	useUnifiedTopology : true,
+	useFindAndModify   : false
 });
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", () => {
-  console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+	console.log('Database connected');
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  touchAfter: 24 * 60 * 60,
-  crypto: {
-    secret,
-  },
+	mongoUrl   : dbUrl,
+	touchAfter : 24 * 60 * 60,
+	crypto     : {
+		secret
+	}
 });
 
 const sessionOptions = {
-  store,
-  name: "in_mkto", // set random name for sessionID
-  secret,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    // secure: true,    // this should only turn on when we use HTTPS
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 1,
-    maxAge: 1000 * 60 * 60 * 24 * 1,
-  },
+	store,
+	name              : 'in_mkto', // set random name for sessionID
+	secret,
+	resave            : false,
+	saveUninitialized : true,
+	cookie            : {
+		httpOnly : true,
+		// secure: true,    // this should only turn on when we use HTTPS
+		expires  : Date.now() + 1000 * 60 * 60 * 24 * 1,
+		maxAge   : 1000 * 60 * 60 * 24 * 1
+	}
 };
 app.use(session(sessionOptions));
 app.use(flash());
@@ -74,87 +76,88 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  if (!["/user/login", "/"].includes(req.originalUrl)) {
-    req.session.returnTo = req.originalUrl;
-  }
-  res.locals.currentUrl = req.originalUrl;
-  res.locals.adminId = adminId;
-  res.locals.currentUser = req.user;
-  res.locals.success = req.flash("success");
-  res.locals.error = req.flash("error");
-  next();
+	if (![ '/user/login', '/' ].includes(req.originalUrl)) {
+		req.session.returnTo = req.originalUrl;
+	}
+	res.locals.currentUrl = req.originalUrl;
+	res.locals.adminId = adminId;
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 app.use(
-  mongoSanitize({
-    replaceWith: "_",
-  })
+	mongoSanitize({
+		replaceWith : '_'
+	})
 );
 
 app.use(helmet());
 const scriptSrcUrls = [
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://api.mapbox.com/",
-  "https://kit.fontawesome.com/",
-  "https://cdnjs.cloudflare.com/",
-  "https://cdn.jsdelivr.net",
-  "https://cdn.tiny.cloud/",
+	'https://stackpath.bootstrapcdn.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
+	'https://cdn.jsdelivr.net',
+	'https://cdn.tiny.cloud/',
+	'https://widgets.rubic.exchange/',
+	'https://app.rubic.exchange',
+	'https://api.harmony.one/',
+	'https://my.rtmark.net/'
 ];
 const styleSrcUrls = [
-  "https://kit-free.fontawesome.com/",
-  "https://stackpath.bootstrapcdn.com/",
-  "https://api.mapbox.com/",
-  "https://api.tiles.mapbox.com/",
-  "https://fonts.googleapis.com/",
-  "https://use.fontawesome.com/",
-  "https://cdn.jsdelivr.net",
-  "https://cdn.tiny.cloud/",
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/',
+	'https://cdn.jsdelivr.net',
+	'https://cdn.tiny.cloud/'
 ];
-const connectSrcUrls = [
-  "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
-  "https://events.mapbox.com/",
+const defaultSrcUrls = [
+	'https://app.rubic.exchange',
+	'https://api.harmony.one/',
+	'https://my.rtmark.net/',
+	'https://widgets.rubic.exchange/'
 ];
-const fontSrcUrls = ["https://fonts.gstatic.com"];
+const connectSrcUrls = [];
+const fontSrcUrls = [ 'https://fonts.gstatic.com' ];
 app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: [],
-      connectSrc: ["'self'", ...connectSrcUrls],
-      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-      workerSrc: ["'self'", "blob:"],
-      objectSrc: [],
-      imgSrc: [
-        "'self'",
-        "blob:",
-        "data:",
-        "https://res.cloudinary.com/",
-        "https://images.unsplash.com/",
-        "https://sp.tinymce.com/",
-      ],
-      fontSrc: ["'self'", ...fontSrcUrls],
-    },
-  })
+	helmet.contentSecurityPolicy({
+		directives : {
+			defaultSrc : defaultSrcUrls,
+			connectSrc : [ "'self'", ...connectSrcUrls ],
+			scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+			styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+			workerSrc  : [ "'self'", 'blob:' ],
+			objectSrc  : [],
+			imgSrc     : [
+				"'self'",
+				'blob:',
+				'data:',
+				'https://res.cloudinary.com/',
+				'https://images.unsplash.com/',
+				'https://sp.tinymce.com/'
+			],
+			fontSrc    : [ "'self'", ...fontSrcUrls ]
+		}
+	})
 );
 
-app.use("/", mainRoutes);
+app.use('/', mainRoutes);
 app.use('/user', userRoutes);
-app.use("/news", articleRoutes);
-app.use("/api/circulating-supply", apiRoutes);
+app.use('/news', articleRoutes);
+app.use('/api/circulating-supply', apiRoutes);
 
-app.all("*", (req, res, next) => {
-  next(new ExpressError("Page not found!", 404));
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page not found!', 404));
 });
 
 app.use((err, req, res, next) => {
-  const { message = "Something went wrong!", status = 500 } = err;
-  res.status(status).render("error", { req, err });
+	const { message = 'Something went wrong!', status = 500 } = err;
+	res.status(status).render('error', { req, err });
 });
 
 const port = process.env.PORT || 8085;
 app.listen(port, () => {
-  console.log(`APP IS LISTENING ON PORT ${port}...`);
+	console.log(`APP IS LISTENING ON PORT ${port}...`);
 });
